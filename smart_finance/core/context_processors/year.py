4f9bb1912e6165e1bb1f30757a2e9cabf896e98a
MrR <you@example.com>
2024-11-05 21:38:34 +0300

    Delete

--- smart_finance/core/context_processors/year.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-
-def year(request):
-    """Добавляет переменную с текущим годом."""
-    date_year = datetime.now().year
-    return {'year': date_year}
--- smart_finance/posts/utils.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.core.paginator import Paginator
-
-
-def get_page(queryset, request, number_of_posts=settings.COUNT_POST):
-    return Paginator(queryset, number_of_posts).get_page(
-        request.GET.get("page")
-    )
